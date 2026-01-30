592512a73e60f0185809b9615e0567c6c433afbd
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-01-30 13:39:34 +0500

    style: About and home

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -4,13 +4,15 @@ import Rasm1 from "../assets/photo_2025-05-27_18-40-45.jpg";
 function About() {
   return (
     <div className="About_container">
-      <div className="About_img">
-        <img src={Rasm1} alt="#" />
+      <div className="About_navbar">
+        <div className="About_navbar_img">
+          <img src={Rasm1} alt="#" />
+        </div>
       </div>
-      <div className="About_text">
-        <h2>About Me</h2>
-        <span> Who Am I? </span>
-        <p>
+      <div className="About_section">
+        <h2 className="About_title">About Me</h2>
+        <span className="About_span"> Who Am I? </span>
+        <p className="About_section_text">
           I am a passionate frontend developer with experience in creating
           responsive and user-friendly web applications. My expertise includes
           React, JavaScript, and modern CSS frameworks.
