ca81861121058313283e1c8a3d87a29d49f1c892
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-06-20 00:19:23 +0500

    icon ozgartirildi

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ export const metadata = {
   title: "Muxriddin Toshboyev - Portfolio",
   description: "Front-end Developer Portfolio",
   icons: {
-    icon: "https://velog.velcdn.com/images/jh0152park/post/3cf084b5-8faa-4875-9267-9d32a1d729ec/image.png",
+    icon: "/Next-icon.png",
   },
 };
 
