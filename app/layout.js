e532e7db0989a32183b0b2b963d1a1f1b0fbc39e
Muxriddin Toshboyev <167794484+MukhriddinToshboyev@users.noreply.github.com>
2026-06-19 23:22:18 +0500

    icon yangilandi

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,14 @@ import "./globals.css";
 export const metadata = {
   title: "Muxriddin Toshboyev - Portfolio",
   description: "Front-end Developer Portfolio",
+  icons: {
+    icon: "https://velog.velcdn.com/images/jh0152park/post/3cf084b5-8faa-4875-9267-9d32a1d729ec/image.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-      </head>
       <body>
         <ThemeProvider attribute="data-theme" defaultTheme="dark">
           {children}
